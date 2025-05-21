689b9d81986339b63a25bcd638b1e40a031116e2
Kshitij Chaturvedi <kshitijhrms@gmail.com>
2025-05-22 04:40:37 +0530

    populate_db done

--- 01_populate_db.py
+++ 01_populate_db.py
@@ -5,6 +5,10 @@ import traceback
 import shutil
 import argparse
 from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from get_embedding_func import embedding_function
+from langchain_chroma import Chroma
 
 
 # load parameters from "params.yaml"
@@ -40,32 +44,147 @@ def setup_logger(name, log_file):
     return logger
 
 
-# load game rules data
 def load_docs(logger):
     try:
+        logger.info("*******************Loading docs from the directory*******************")
+        
         doc_loader = PyPDFDirectoryLoader(GAME_RULES_DATA_PATH, glob="*.pdf")   
-        logger.info(f"Loading documents from {GAME_RULES_DATA_PATH}")
+        logger.info(f"[Stage 1] Loading documents from {GAME_RULES_DATA_PATH}")
+        
+        logger.info("*******************Docs loaded successfully*******************")
         return doc_loader.load()
     except Exception as e:
         logger.error(f"Error loading documents: {e}")
         logger.debug(traceback.format_exc())
         return []
 
+def split_docs(docs: list[Document], logger):
+    try:
+        logger.info("*******************Splitting docs into chunks*******************")
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=CHUNK_SIZE,
+            chunk_overlap=CHUNK_OVERLAP,
+            length_function=len,
+            is_separator_regex=False,
+        )
+        logger.info("[Stage 2] Splitting documents into chunks")
+        
+        logger.info("*******************Chunks created successfully*******************")
+        return text_splitter.split_documents(docs)
+    except Exception as e:
+        logger.error(f"Error splitting documents: {e}")
+        logger.debug(traceback.format_exc())
+        return []
+
+# logic to add & update items in the existing db
+def save_to_chroma_db(chunks: list[Document], logger):
+    try:
+        logger.info("*******************Saving chunks to Chroma DB*******************")
+        
+        # load the existing db
+        db = Chroma(
+            embedding_function=embedding_function(),
+            persist_directory=CHROMA_DB_PATH,
+        )
+        logger.info(f"[Stage 3.1] Loading existing DB from path: {CHROMA_DB_PATH}")
+        
+        # calculate "page:chunk" IDs
+        chunks_with_ids = calc_chunk_ids(chunks)
+        logger.info(f"[Stage 3.2] Calculated chunk IDs for {len(chunks_with_ids)} chunks")
+        
+        # add/update the docs
+        existing_items = db.get(include=[]) # IDs are always included by default
+        existing_ids = set(existing_items["ids"])
+        logger.info(f"[Stage 3.3] No. of existing items (i.e. docs) in the db: {len(existing_ids)}")
+        
+        # only add docs that don't exist in the db
+        new_chunks = []
+        for chunk in chunks_with_ids:
+            if chunk.metadata["chunk_id"] not in existing_ids:
+                new_chunks.append(chunk)
+        logger.info(f"[Stage 3.4] No. of new chunks to add: {len(new_chunks)}")
+        
+        if len(new_chunks):
+            logger.info(f"[Stage 3.5(a)] Adding {len(new_chunks)} new chunks to the db")
+            new_chunk_ids = [chunk.metadata["chunk_id"] for chunk in new_chunks]
+            db.add_documents(new_chunks, ids=new_chunk_ids)
+        else:
+            logger.info("[Stage 3.5(b)] No new chunks to add to the db")
+        
+        logger.info("*******************Chunks saved to Chroma DB successfully*******************")        
+    except Exception as e:
+        logger.error(f"Error saving to Chroma DB: {e}")
+        logger.debug(traceback.format_exc())
+        return []
+
+def calc_chunk_ids(chunks):
+    # Page Source : Page Number : Chunk Index
+    last_page_id = None
+    curr_chunk_idx = 0
+    
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        curr_page_id = (f"{source}:{page}")
+        
+        # if the page ID is the same as the last one, increment the index
+        if curr_page_id == last_page_id:
+            curr_chunk_idx += 1
+        else:
+            curr_chunk_idx = 0
+        
+        # calculate the new chunk ID
+        chunk_id = (f"{curr_page_id}:{curr_chunk_idx}")
+        last_page_id = curr_page_id
+        
+        # add the chunk ID to the metadata
+        chunk.metadata["chunk_id"] = chunk_id
+    
+    return chunks
+
+def clear_database():
+    if os.path.exists(CHROMA_DB_PATH):
+        shutil.rmtree(CHROMA_DB_PATH)
+
 
 def main():
-    logger = setup_logger("create_db_logger", LOG_FILE)
-    logger.info("*******************Starting db population*******************")
-    
-    logger.info("*******************Loading docs from the directory*******************")
-    documents = load_docs(logger)
-    logger.info(f"Loaded {len(documents)} documents")
-    if not documents:
-        logger.error("No documents loaded. Exiting.")
-        return
-    logger.info(f"First document: {documents[0]}")
-    logger.info("*******************Docs loaded successfully*******************")
-    
-    logger.info("*******************DB population completed*******************")
+    try:
+        logger = setup_logger("create_db_logger", LOG_FILE)
+        logger.info(" ")
+        logger.info("*******************Starting db population*******************")
+        
+        # check if the db should be cleared (using the --clear flag)
+        parser = argparse.ArgumentParser(description="Populate the database")
+        parser.add_argument("--reset", action="store_true", help="Reset the database.")
+        args = parser.parse_args()
+        if args.reset:
+            logger.info("Clearing the database...")
+            clear_database()
+        
+        
+        # create (or update) the db
+        documents = load_docs(logger)
+        # logger.info(f"Loaded {len(documents)} documents")
+        if not documents:
+            logger.error("No documents loaded. Exiting.")
+            return
+        # logger.info(f"First document: {documents[0]}")
+        
+        chunks = split_docs(documents, logger)
+        # logger.info(f"Split into {len(chunks)} chunks")
+        if not chunks:
+            logger.error("No chunks created. Exiting.")
+            return
+        # logger.info(f"First chunk: {chunks[0]}")
+        
+        save_to_chroma_db(chunks, logger)
+        
+        
+        logger.info("*******************DB population completed*******************")
+    except Exception as e:
+        logger.error(f"Error in main: {e}")
+        logger.debug(traceback.format_exc())
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # rag_ex2
 
-### [v1](https://github.com/Kshitijk14/rag_ex)
+### Rag Ex. V1 Link: [rag_ex v1](https://github.com/Kshitijk14/rag_ex)
 
 ## Key tasks:
     1. dynamically update db
